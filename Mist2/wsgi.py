a2ff1912d64d69673feaab35c110e9cb1f82393b
Momchil Litov <litovmomchil@gmail.com>
2024-10-22 21:11:12 +0000

    Final changes

--- Mist2/wsgi.py
+++ Mist2/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mist2.settings')
+settings_module = 'azure_project.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'azure_project.settings'
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
